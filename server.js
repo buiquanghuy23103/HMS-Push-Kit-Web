c6b53baa74e6e000410fc9851c109d9b216e6886
buiquanghuy23103 <huythdnbkltv@gmail.com>
2020-08-11 15:25:06 +0300

    Get access token with CORS error

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 let express = require('express');
-let http = require('http')
-let crypto = require("crypto")
-
+let http = require('http');
+let cors = require("cors");
+let cookieParser = require("cookie-parser");
 
 let app = express();
 let port = 5060;
 
 app.use("/webpush", express.static("./",{index:"index.html"}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
 http.createServer(app).listen(port, (error) => {
     if (error) {
